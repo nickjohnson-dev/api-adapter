dbb9cc287f23a252132d7a5e3c2acec8623b4de3
Nick Johnson <nickjohnson.dev@gmail.com>
2017-02-10 21:50:21 -0600

    Basics are working, but it's ugly

--- src/create-adapter.js
+++ src/create-adapter.js
@@ -1,5 +1,29 @@
-const createAdapter = propertyMap => ({
-  propertyMap,
+import { get, includes, invert, isUndefined, keys, omitBy, set } from 'lodash/fp';
+
+const createAdapter = keyMap => ({
+  fromApi: apiObj => swapPaths(keyMap, apiObj),
+  toApi: clientObj => swapPaths(invert(keyMap), clientObj),
 });
+
+function swapPaths(pathMap, source) {
+  const mappedObj = keys(pathMap).reduce((acc, path) => {
+    const value = get(path, source);
+
+    if (isUndefined(value)) return acc;
+
+    return set(pathMap[path], value, acc);
+  }, {});
+
+  const flatPaths = keys(pathMap).map(key =>
+    (includes('.', key) ? key.split('.')[0] : key),
+  );
+
+  const leftovers = omitBy((value, key) => includes(key, flatPaths), source);
+
+  return {
+    ...mappedObj,
+    ...leftovers,
+  };
+}
 
 export default createAdapter;
--- src/create-adapter.test.js
+++ src/create-adapter.test.js
@@ -1,10 +1,101 @@
 import test from 'ava';
 import createAdapter from './create-adapter';
 
-test('createAdapter should return object containing input property map', (t) => {
-  const propertyMap = {
-    a: 'b',
+test('fromApi should handle deep object', (t) => {
+  const keyMap = {
+    digits: 'phone.number',
+    phonePrefix: 'phone.areaCode',
+    knownBy: 'name',
+    'residenceIdentifier.road': 'address.street',
+    'residenceIdentifier.postalCode': 'address.zip',
   };
-  const result = createAdapter(propertyMap);
-  t.deepEqual(result.propertyMap, propertyMap);
+  const adapter = createAdapter(keyMap);
+  const user = {
+    digits: '555-1234',
+    knownBy: 'Nick',
+    phonePrefix: '123',
+    residenceIdentifier: {
+      road: 'Main Street',
+      postalCode: 90210,
+    },
+    untouched: true,
+  };
+  const expected = {
+    address: {
+      street: 'Main Street',
+      zip: 90210,
+    },
+    name: 'Nick',
+    phone: {
+      areaCode: '123',
+      number: '555-1234',
+    },
+    untouched: true,
+  };
+  const result = adapter.fromApi(user);
+  t.deepEqual(result, expected);
 });
+
+test('toApi should handle deep object', (t) => {
+  const keyMap = {
+    digits: 'phone.number',
+    phonePrefix: 'phone.areaCode',
+    knownBy: 'name',
+    'residenceIdentifier.road': 'address.street',
+    'residenceIdentifier.postalCode': 'address.zip',
+  };
+  const adapter = createAdapter(keyMap);
+  const user = {
+    address: {
+      street: 'Main Street',
+      zip: 90210,
+    },
+    name: 'Nick',
+    phone: {
+      areaCode: '123',
+      number: '555-1234',
+    },
+    untouched: true,
+  };
+  const expected = {
+    digits: '555-1234',
+    knownBy: 'Nick',
+    phonePrefix: '123',
+    residenceIdentifier: {
+      road: 'Main Street',
+      postalCode: 90210,
+    },
+    untouched: true,
+  };
+  const result = adapter.toApi(user);
+  t.deepEqual(result, expected);
+});
+
+test.todo('should carry through properties that do not exist on keyMap');
+test.todo('fromApi single level');
+test.todo('toApi');
+
+// eslint-disable-next-line
+// test.todo('toApi should return input object with keys renamed from client keys to api keys', (t) => {
+//   const keyMap = {
+//     first: 'ichiban',
+//     second: 'niban',
+//     third: 'sanban',
+//     fourth: 'shiban',
+//   };
+//   const adapter = createAdapter(keyMap);
+//   const favoriteRamen = {
+//     ichiban: 'Tonkotsu',
+//     niban: 'Miso',
+//     sanban: 'Shio',
+//     shiban: 'Shoyu',
+//   };
+//   const expected = {
+//     first: 'Tonkotsu',
+//     second: 'Miso',
+//     third: 'Shio',
+//     fourth: 'Shoyu',
+//   };
+//   const result = adapter.toApi(favoriteRamen);
+//   t.deepEqual(result, expected);
+// });
